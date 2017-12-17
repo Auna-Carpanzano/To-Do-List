80ae05ad10941d599b9f0707948e1be65a99b294
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-17 14:14:01 -0700

    Refactor the if/else statement

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 $("li").click(function () {
   if ($(this).css("color") === "rgb(128, 128, 128)") {
-    $(this).css("color", "black");
-    $(this).css("text-decoration", "none");
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
   }
   else {
-    $(this).css("color", "gray");
-    $(this).css("text-decoration", "line-through");
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
   }
 });
