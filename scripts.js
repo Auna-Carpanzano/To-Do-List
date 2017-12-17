d44a92db4456843c7f8e713e9a792cd98279df4b
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-17 15:00:33 -0700

    Change "click" to be "on click" to apply to new list items

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
@@ -13,6 +13,6 @@ $("input[type='text']").keypress(function(e){
   if(e.which === 13){
     var inputText= $(this).val();
     $(this).val("");
-    $("ul").append("<li>" + inputText + "</li>");
+    $("ul").append("<li><span>Delete</span> " + inputText + "</li>");
   }
 });
